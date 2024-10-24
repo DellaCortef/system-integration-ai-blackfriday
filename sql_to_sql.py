041377aefaa97ffff34137af18ef335903fda525
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-10-24 19:27:58 -0300

    creating my engine_destiny and my engine_origin

--- sql_to_sql.py
+++ sql_to_sql.py
@@ -14,13 +14,14 @@ database_url     = os.getenv("DATABASE_URL")
 database_url_aws = os.getenv("DATABASE_URL_AWS")
 table            = os.getenv("TABLE_NAME")
 
-engine = create_engine(database_url)
+engine_origin  = create_engine(database_url)
+engine_destiny = create_engine(database_url_aws)
 
 # Execute an SQL query and load the results into a DataFrame
 query = f"SELECT * FROM {table}"
-df = pd.read_sql_query(query, engine)
+df = pd.read_sql_query(query, engine_origin)
 
 # Saving DataFrame to a table in my new DataBase
-df.to_sql(table, engine, if_exists='replace', index=False)
+df.to_sql(table, engine_destiny, if_exists='replace', index=False)
 
 print("CSV created successfully!")
