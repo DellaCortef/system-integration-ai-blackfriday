43ca2bd69fa4a31af6ce1eadb5544a1de7508b21
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-10-24 18:58:54 -0300

    creating engine that will connect to my database

--- sql_to_csv.py
+++ sql_to_csv.py
@@ -4,4 +4,13 @@
 import os
 import pandas as pd
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the database URL and table name from environment variables
+database_url = os.getenv("DATABASE_URL")
+table_name   = os.getenv("TABLE_NAME")
+
+engine = create_engine(database_url)
