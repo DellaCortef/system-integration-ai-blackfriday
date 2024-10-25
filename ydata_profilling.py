af10d41e161efb22f2fd1f7b78c6f0c9dc16bb5a
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-10-24 21:21:56 -0300

    adding Profile Report for typeform_responses

---
+++ ydata_profilling.py
@@ -0,0 +1,19 @@
+# using pandas read_sql() to read data from a database table and save 
+# it in .csv
+
+import os
+import pandas as pd
+from dotenv import load_dotenv
+from ydata_profilling import ProfileReport
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Read csv file
+df = pd.read_csv("typeform_responses.csv")
+
+# Instantiating the profile_report() method
+profile = ProfileReport(df, title="Profilling Report")
+
+profile.to_file("typeform_report.html")
+
